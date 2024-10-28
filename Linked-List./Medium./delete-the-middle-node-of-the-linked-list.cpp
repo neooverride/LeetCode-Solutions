29a2fd26438b708d2bfe20fa156e5d118efde682
x4ldr1t89z <alishbaabdulstealth@gmail.com>
2024-10-28 21:02:00 +0500

    Problems.

---
+++ Linked-List./Medium./delete-the-middle-node-of-the-linked-list.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (!head || !head->next) return nullptr;
+
+        ListNode* slowPointer = head;
+        ListNode* fastPointer = head;
+        ListNode* prev = nullptr;
+
+        while (fastPointer != nullptr && fastPointer->next != nullptr) {
+            prev = slowPointer;
+            slowPointer = slowPointer->next;
+            fastPointer = fastPointer->next->next;
+        }
+        prev->next = slowPointer->next;
+        delete slowPointer;
+
+        return head;
+    }
+};
